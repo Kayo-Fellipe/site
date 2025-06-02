f2096a83619c9f44b30aef3dffa9cd5043755a35
Kayo Fellipe <167824869+Kayo-Fellipe@users.noreply.github.com>
2025-06-02 14:02:54 -0300

    Meu site

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 // This file imports all JavaScript modules
 // Import the main JavaScript files
+import './carousel.js';
+import './form-contact.js';
+import './form-depoimento.js';
 import './main.js';
+import './mockData.js';
 import './portfolio.js';
 import './testimonials.js';
-import './form.js';
 import './thankyou.js';
 
 // Initialize the application
